a102eace712df94add8107e710db88a67efacf6f
abdullahnadeem0 <codewithabdullahnadeem@gmail.com>
2026-05-04 17:10:03 +0500

    dhfd

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,9 @@
 import React from 'react'
 import "./Home.css"
 import { FaAngleDown } from "react-icons/fa";
+import { CiSearch } from "react-icons/ci";
+import img3 from "../imgs/Banner 1.png"
+import { HiOutlineAdjustmentsHorizontal } from "react-icons/hi2";
 
 const Home = () => {
   return (
@@ -12,6 +15,22 @@ const Home = () => {
          <h4 className='Location'>Bilzen, Tanjungbalai  <span className='Location-icon'><FaAngleDown /></span> 
  </h4>
      </div>
+
+      <div className='home-search-contaner'>
+        <div className='serach-bar'>
+             <button className='serach-btn-home'><CiSearch /></button>
+             <input type="text" placeholder='Serach coffee'  />
+        </div>
+        <button className='adjast-icon'><HiOutlineAdjustmentsHorizontal />
+</button>
+      </div>
+
+    <div className='Buy-one-get'>
+       <img src={img3} alt="" />
+    </div>
+
+
+
  </div>
      
     </>
