f2dae201f9eba782dd621fc22d25fe743d625daf
abdullahnadeem0 <codewithabdullahnadeem@gmail.com>
2026-05-04 14:26:01 +0500

    fjfjkrfjkr

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,11 +1,19 @@
 import React from "react";
-
+import lad from "../imgs/Image Onboarding (1).png";
+import img1 from "../imgs/Shadow Container.png";
+import "./Landing.css"
 const Landing = () => {
   return (
-    <div style={{ textAlign: "center", padding: "50px" }}>
-      <h1>☕ Coffee Landing Page</h1>
-      <p>Best coffee experience starts here</p>
-    </div>
+    <>
+    <div className="landing-main-canterner">
+   <img className="landing-img" src={lad} alt="image" />
+   <img className="landing-Shadow" src={img1} alt="image" />
+   
+   <div className="landing-contant">
+
+   </div>
+   </div>
+    </>
   );
 };
 
