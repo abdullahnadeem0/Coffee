584db7b62c9d42c5093b28f64efcdac1c5446e5b
abdullahnadeem0 <codewithabdullahnadeem@gmail.com>
2026-05-05 18:54:10 +0500

    dfjidfjidf

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Home from "./pages/Home";
-
+import Detail from "./pages/Detail";
 const App = () => {
   return (
     <Router>
@@ -10,6 +10,7 @@ const App = () => {
 
         <Route path="/" element={<Landing />} />
         <Route path="/home" element={<Home/>} />
+        <Route path="/detail" element={<Detail/>} />
 
       </Routes>
     </Router>
---
+++ src/pages/Detail.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+ import "./Detail.css"
+ import { FaAngleLeft } from "react-icons/fa6";
+import { TiHeartOutline } from "react-icons/ti";
+ import img5 from  "../imgs/Image.png"
+import { RiEBike2Fill } from "react-icons/ri";
+import { GiCoffeeBeans } from "react-icons/gi";
+import { TfiPackage } from "react-icons/tfi";
+
+const Detail = () => {
+  return (
+     <>
+      <div className='Detail-navbar'> 
+            <span className='Back-icon'> <FaAngleLeft /></span>
+            <h2 className='Detail-titel'>Detail</h2>
+            <span className='wislist-Detail'><TiHeartOutline /></span> </div>
+      
+
+          <div className='Detail-div'>
+
+            <div className='Detail-main-img'>
+                
+                 <img src={img5} alt="" />
+                </div> 
+
+                 <h2 className='produt-Detail-titel'> Caffe Mocha</h2>
+
+                 <div className='delivery'>
+                    <p>Ice/Hot</p>
+                     <div className='delivery-icon'>
+                        <span className='delivery-icon-style'> <RiEBike2Fill /> </span>
+                        <span className='delivery-icon-style'> <GiCoffeeBeans /> </span>
+                        <span className='delivery-icon-style'> <TfiPackage /> </span>
+                     </div>
+                 </div>
+                 <hr className='Detali-line' />
+
+                 <h2 className='Description-titel'>Description</h2>
+                  <p className='Description-para'>A cappuccino is an approximately 150 ml (5 oz)<br/> beverage, with 25 ml of espresso coffee and<br/> 85ml of fresh milk the fo..  <span className='bold-active'>Read More</span> </p>
+         
+                     <div className='Size'>
+                         <h2 className='Size-titel'> Size</h2>
+                        <div className='size-btn-all'>
+                        <button>S</button>
+                        <button className='Size-cative'>M</button>
+                        <button>L</button>
+                        </div>
+                     </div>
+         
+          </div>
+
+     </>
+  )
+}
+
+export default Detail
